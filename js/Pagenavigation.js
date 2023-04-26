c4f7b309445b2019c44783f62ece39bf6dc02289
Benjamin <Good435lovesyou@gmail.com>
2023-04-25 19:50:52 -0600

    webpage remake

--- js/Pagenavigation.js
+++
@@ -1,7 +0,0 @@
-const handleClick = (e) => {
-  e.preventDefault();
-  const target = e.target.getAttribute("href");
-  console.log(target);
-  const element = document.querySelector(target);
-  element.scrollIntoView({ behavior: "smooth" });
-}
